8675faf3d177469cf4a7b325c4274fdb2f38dbc7
Biraj Koirala <koiralabiraj@gmail.com>
2025-02-13 18:13:04 +0700

    bumo version v0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rupantaran",
-    version="0.1.0",
+    version="0.2.1",
     packages=find_packages(),
     license="MIT",
     description="Rupantaran is a Python package that converts various Nepali-specific measurements into SI units or commonly used metric units.",
